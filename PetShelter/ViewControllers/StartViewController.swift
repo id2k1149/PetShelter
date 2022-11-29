911c32b1744de3d1fb33db4fc80a87d3c04e4f3e
id2k1149 <id2k1149@gmail.com>
2022-11-28 20:48:49 -0500

    edit title size

--- PetShelter/ViewControllers/StartViewController.swift
+++ PetShelter/ViewControllers/StartViewController.swift
@@ -21,19 +21,17 @@ class StartViewController: UIViewController {
         guard let startBackgound = UIImage(named: "start-screen-background") else { return }
         stackOne.backgroundColor = UIColor(patternImage: startBackgound)
         
-
-//        navigationController?.navigationBar.topItem?.titleView?.tintColor = UIColor.systemYellow
-       
-        
-        
-        
         // This will change the navigation bar background color
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = UIColor(patternImage: startBackgound)
         appearance.titlePositionAdjustment.horizontal = CGFloat(-250)
-        appearance.titlePositionAdjustment.vertical = CGFloat(-4)
-        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.systemYellow]
+        appearance.titlePositionAdjustment.vertical = CGFloat(-8)
+       
+        appearance.titleTextAttributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 30),
+            NSAttributedString.Key.foregroundColor: UIColor.systemYellow
+        ]
 
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
