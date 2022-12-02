3bcfeaccd98715cc48f476bd020a2fde808275c4
id2k1149 <id2k1149@gmail.com>
2022-12-01 20:31:29 -0500

    edit cells size

--- PetShelter/ViewControllers/GalleryCollectionView.swift
+++ PetShelter/ViewControllers/GalleryCollectionView.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 class GalleryCollectionView: UICollectionView,
-                                UICollectionViewDelegate,
-                             UICollectionViewDataSource {
+                             UICollectionViewDelegate,
+                             UICollectionViewDataSource,
+                             UICollectionViewDelegateFlowLayout
+{
     
     var cells = [Pet]()
     
@@ -26,45 +28,50 @@ class GalleryCollectionView: UICollectionView,
         
         translatesAutoresizingMaskIntoConstraints = false
         
-//        backgroundColor = #colorLiteral(red: 0.9589126706, green: 0.9690223336, blue: 0.9815708995, alpha: 1)
-//        delegate = self
-//        dataSource = self
-//
-//
-//        translatesAutoresizingMaskIntoConstraints = false
-//        layout.minimumLineSpacing = Constants.galleryMinimumLineSpacing
-//        contentInset = UIEdgeInsets(top: 0, left: Constants.leftDistanceToView, bottom: 0, right: Constants.rightDistanceToView)
-//
-//
-//        showsHorizontalScrollIndicator = false
-//        showsVerticalScrollIndicator = false
+        //        backgroundColor = #colorLiteral(red: 0.9589126706, green: 0.9690223336, blue: 0.9815708995, alpha: 1)
+        //        delegate = self
+        //        dataSource = self
+        //
+        //
+        //        translatesAutoresizingMaskIntoConstraints = false
+        //        layout.minimumLineSpacing = Constants.galleryMinimumLineSpacing
+        //        contentInset = UIEdgeInsets(top: 0, left: Constants.leftDistanceToView, bottom: 0, right: Constants.rightDistanceToView)
+        //
+        //
+        //        showsHorizontalScrollIndicator = false
+        //        showsVerticalScrollIndicator = false
         
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        cells.count
-        print("5 cells")
-        return 5
+        //        cells.count
+        5
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.reuseId, for: indexPath)
-//        cell.mainImageView.image = cells[indexPath.row].image
-//                cell.nameLabel.text = cells[indexPath.row].sushiName
-//                cell.smallDescriptionLabel.text = cells[indexPath.row].smallDescription
-//                cell.costLabel.text = "$\(cells[indexPath.row].cost)"
-                return cell
+        //        cell.mainImageView.image = cells[indexPath.row].image
+        //                cell.nameLabel.text = cells[indexPath.row].sushiName
+        //                cell.smallDescriptionLabel.text = cells[indexPath.row].smallDescription
+        //                cell.costLabel.text = "$\(cells[indexPath.row].cost)"
+        return cell
         
     }
     
-   
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-//    func set(cells: [Pet]) {
-//        self.cells = cells
-//    }
-
+    //    func set(cells: [Pet]) {
+    //        self.cells = cells
+    //    }
+    
 }
